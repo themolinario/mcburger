c1680aa27558bc8e66cd0501182709bbc49f40e1
Marco Molinari <themolinario@g,mail.com>
2022-06-23 17:34:37 +0200

    update3

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,5 +1,7 @@
 import * as React from "react";
 import ResponsiveAppBarLogin from "../components/ResponsiveAppBarLogin";
+import Container from "@mui/material/Container";
+import '../index.css';
 
 function Home() {
   return (
@@ -7,6 +9,13 @@ function Home() {
       <div>
         <ResponsiveAppBarLogin className="NavBar"/>
       </div>
+      <Container className="HomeContainer" maxWidth="lg">
+        <h1>Benvenuto su McBurger!</h1>
+        <h2>Scopri il nostro Menu:</h2>
+        <div>
+
+        </div>
+      </Container>
     </div>
 
   )
