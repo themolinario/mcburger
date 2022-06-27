29ba8d55ba6ed8d070d96d25d98cbcdacc356e1d
aleatta <alexattanasio8@gmail.com>
2022-06-27 21:00:39 +0200

    update14

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,11 +2,10 @@ import * as React from "react";
 import ResponsiveAppBarLogin from "../components/ResponsiveAppBarLogin";
 import Container from "@mui/material/Container";
 import '../index.css';
-import Button from "@mui/material/Button";
 import { useNavigate } from "react-router-dom";
+import HomeButton from "../components/HomeButton";
 
 function Home() {
-  const navigate = useNavigate();
   return (
     <div className="Home">
       <div>
@@ -16,15 +15,22 @@ function Home() {
         <h1 id="Welcome">Benvenuto su McBurger!</h1>
         <h2 id="Welcome2">Scopri il nostro Menu:</h2>
         <div>
-          <Button id="MenuButton" variant="contained" color="secondary" onClick={() => {
-            navigate('./Menu');
-          }}>Menu</Button>
-        </div>
-        <div>
-          
+          <HomeButton/>
         </div>
       </Container>
+      <Container className="HomeContainer" maxWidth="lg">
+        <h2 id="Welcome3"><a href="/login">Accedi</a> oppure <a href="/signup">Registrati</a> per usufruire dei servizi offerti da Mc Burger </h2>
+        <h3 id="Welcome3"><a href="/support">Contattaci</a> per informazioni</h3>
+      </Container>
+      <div>
+        <Container className="HomeContainer" maxWidth="lg">
+          <footer id="Welcome4">
+           Copyright Mc Burger© 2022. All rights reserved
+          </footer>
+        </Container>
+      </div>
     </div>
+
 
   )
 }
